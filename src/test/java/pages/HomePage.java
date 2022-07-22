dcad5d87bcc707d672fb071998ec46495d2bf9cc
Banditphak <banditphak@hotmail.com>
2022-07-21 22:32:23 -0400

    Testimonials Test add path in home page

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utils.WebDriverManager;
@@ -8,6 +9,19 @@ public class HomePage {
     public HomePage(){
         PageFactory.initElements(WebDriverManager.getDriver(), this);
     }
+
+    @FindBy( xpath = "//div/h2[contains(text(),'Words from our Clients')]")
+    public WebElement clientsHeader;
+
+    @FindBy(xpath = "//div[@class='testimonial-block-one']/div[@class='inner-box']/div[@class='text']")
+    public WebElement clientsWordsText;
+
+    @FindBy(xpath = "//div[@class='thumb-content']/h3")
+    public WebElement clientsName;
+
+    @FindBy(xpath = "//div[@class='thumb-content']/div")
+    public WebElement clientsState;
+
 
 }
 
