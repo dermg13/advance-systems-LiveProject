b7ab676d976cacdf54070087759f0ed4defb7747
ppariya99 <ppariya145@gmail.com>
2022-07-22 17:44:55 -0400

    committed changes

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         glue = {"step_definitions"},
         stepNotifications = true,
         dryRun = false,
-        tags = "@ADVSYS-13"
+        tags = "@ADVSYS-9"
 )
 public class CukesRunner {
 
