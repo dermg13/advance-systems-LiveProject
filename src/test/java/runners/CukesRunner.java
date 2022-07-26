c9f620dbc830382b5111931e2ec81abd83b21f02
Banditphak <banditphak@hotmail.com>
2022-07-25 22:48:27 -0400

    Try fix homeStep for jenkins3

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         glue = {"step_definitions"},
         stepNotifications = true,
         dryRun = false,
-        tags = "@ADVSYS-19"
+        tags = "@ADVSYS19"
 )
 public class CukesRunner {
 
