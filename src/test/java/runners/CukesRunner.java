39851140e59d4e59185dd98c13f30f2f78c72af2
Banditphak <banditphak@hotmail.com>
2022-07-27 20:05:50 -0400

    Notting Again

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         glue = {"step_definitions"},
         stepNotifications = true,
         dryRun = false,
-        tags = "@ADVSYS-5"
+        tags = "@ADVSYS-19"
 )
 public class CukesRunner {
 
