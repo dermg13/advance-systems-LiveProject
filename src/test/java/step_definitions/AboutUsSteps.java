1989063fb2345ea0b5f3f034c666449e0912f8aa
Banditphak <banditphak@hotmail.com>
2022-08-02 16:33:54 -0400

    Final

--- src/test/java/step_definitions/AboutUsSteps.java
+++ src/test/java/step_definitions/AboutUsSteps.java
@@ -5,6 +5,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import pages.AboutUsPage;
 import pages.CommonPage;
@@ -22,7 +23,6 @@ public class AboutUsSteps implements CommonPage {
     public void click_on_about_us_page() {
         WebDriverManager.click(aboutUsPage.AboutUsMainNavigationBar);
     }
-
     @Given("There should be a main header Welcome to Advance Systems LLC.")
     public void there_should_be_a_main_header_welcome_to_advance_systems_llc() {
         Assert.assertEquals(WebDriverManager.getText(aboutUsPage.MainContentPersonName), "Kuba Z");
@@ -33,107 +33,65 @@ public class AboutUsSteps implements CommonPage {
         Assert.assertTrue(WebDriverManager.isDisplayed(aboutUsPage.MainContentPersonName));
         Assert.assertTrue(WebDriverManager.isDisplayed(aboutUsPage.MainContentTitle));
     }
-
     @Then("This section should have a header   Meet Our Experts.")
     public void this_section_should_have_a_header_meet_our_experts() {
         Assert.assertTrue(WebDriverManager.isDisplayed(aboutUsPage.MeetExpertsHeader));
     }
-
     @Then("Total of four employees should be displayed Picture")
-    public void total_of_four_employees_should_be_displayed_picture() {
+    public void total_of_four_employees_should_be_displayed_picture() throws InterruptedException {
         List<WebElement> image = WebDriverManager.getDriver().findElements(By.xpath("//div[@class='team-member-one col-lg-3 col-md-6 col-xs-12']//img[@src]"));
         for (int i = 0; i < image.size(); i++) {
             Assert.assertTrue(WebDriverManager.isDisplayed(image.get(i)));
         }
     }
     @Then("Total of four employees should be displayed Title")
-    public void total_of_four_employees_should_be_displayed_title() {
+    public void total_of_four_employees_should_be_displayed_title() throws InterruptedException {
         List<WebElement> title = WebDriverManager.getDriver().findElements(By.xpath("//div[@class='team-member-one col-lg-3 col-md-6 col-xs-12']//div[@class='lower-content']/div[@class='designation']"));
         for (int i = 0; i < title.size(); i++) {
             Assert.assertTrue(WebDriverManager.isDisplayed(title.get(i)));
         }
     }
     @Then("Total of four employees should be displayed Quote")
-    public void total_of_four_employees_should_be_displayed_quote() {
+    public void total_of_four_employees_should_be_displayed_quote() throws InterruptedException {
         List<WebElement> quote = WebDriverManager.getDriver().findElements(By.xpath("//div[@class='team-member-one col-lg-3 col-md-6 col-xs-12']//div[@class='lower-content']//div[@class='text']"));
         for (int i = 0; i < quote.size(); i++) {
             Assert.assertTrue(WebDriverManager.isDisplayed(quote.get(i)));
         }
     }
     @Then("Total of four employees should be displayed Social media link")
-    public void total_of_four_employees_should_be_displayed_social_media_link() {
+    public void total_of_four_employees_should_be_displayed_social_media_link() throws InterruptedException {
         List<WebElement> social = WebDriverManager.getDriver().findElements(By.xpath("//ul[@class='social-icon-one']"));
         for (int i = 0; i < social.size(); i++) {
             Assert.assertTrue(WebDriverManager.isDisplayed(social.get(i)));
         }
     }
+    @Then("Employees Social media buttons should take to corresponding pages")
+    public void employeed_social_media_buttons_should_take_to_corresponding_pages() throws InterruptedException {
 
-    @Then("All employees FaceBook buttons should take to FaceBook pages")
-    public void all_face_book_buttons_should_take_to_face_book_pages() throws InterruptedException {
-
-            WebDriverManager.getDriver().findElement(By.xpath("//body[1]/div[1]/section[5]/div[1]/div[2]/div[1]/div[1]/div[2]/ul[1]/li[1]/a[1]/span[1]")).click();
-            System.out.println(WebDriverManager.getDriver().getTitle());
-            WebDriverManager.getDriver().navigate().back();
-            WebDriverManager.getDriver().findElement(By.xpath("//body/div[1]/section[5]/div[1]/div[2]/div[2]/div[1]/div[2]/ul[1]/li[1]/a[1]/span[1]")).click();
-            System.out.println(WebDriverManager.getDriver().getTitle());
-            WebDriverManager.getDriver().navigate().back();
-            WebDriverManager.getDriver().findElement(By.xpath("//body/div[1]/section[5]/div[1]/div[2]/div[3]/div[1]/div[2]/ul[1]/li[1]/a[1]/span[1]")).click();
-            System.out.println(WebDriverManager.getDriver().getTitle());
-            WebDriverManager.getDriver().navigate().back();
-            WebDriverManager.getDriver().findElement(By.xpath("//body/div[1]/section[5]/div[1]/div[2]/div[4]/div[1]/div[2]/ul[1]/li[1]/a[1]/span[1]")).click();
+        List<WebElement> facebook = WebDriverManager.getDriver().findElements(By.xpath("//span/ancestor::section//a[@href='https://facebook.com']"));
+        for (int i = 0; i < facebook.size(); i++) {
+            facebook.get(i).click();
             System.out.println(WebDriverManager.getDriver().getTitle());
             WebDriverManager.getDriver().navigate().back();
         }
-
-
-    @Then("All Twitter buttons should take to Twitter pages")
-    public void all_twitter_buttons_should_take_to_twitter_pages() {
-        WebDriverManager.getDriver().findElement(By.xpath("//body[1]/div[1]/section[5]/div[1]/div[2]/div[1]/div[1]/div[2]/ul[1]/li[2]/a[1]/span[1]")).click();
-        System.out.println(WebDriverManager.getDriver().getTitle());
-        WebDriverManager.getDriver().navigate().back();
-        WebDriverManager.getDriver().findElement(By.xpath("//body[1]/div[1]/section[5]/div[1]/div[2]/div[2]/div[1]/div[2]/ul[1]/li[2]/a[1]/span[1]")).click();
-        System.out.println(WebDriverManager.getDriver().getTitle());
-        WebDriverManager.getDriver().navigate().back();
-        WebDriverManager.getDriver().findElement(By.xpath("//body[1]/div[1]/section[5]/div[1]/div[2]/div[3]/div[1]/div[2]/ul[1]/li[2]/a[1]/span[1]")).click();
-        System.out.println(WebDriverManager.getDriver().getTitle());
-        WebDriverManager.getDriver().navigate().back();
-        WebDriverManager.getDriver().findElement(By.xpath("//body[1]/div[1]/section[5]/div[1]/div[2]/div[4]/div[1]/div[2]/ul[1]/li[2]/a[1]/span[1]")).click();
-        System.out.println(WebDriverManager.getDriver().getTitle());
-        WebDriverManager.getDriver().navigate().back();
-    }
-
-    @Then("All Skype buttons should take to Skype pages")
-    public void all_skype_buttons_should_take_to_skype_pages() {
-        WebDriverManager.getDriver().findElement(By.xpath("//body[1]/div[1]/section[5]/div[1]/div[2]/div[1]/div[1]/div[2]/ul[1]/li[3]/a[1]/span[1]")).click();
-        System.out.println(WebDriverManager.getDriver().getTitle());
-        WebDriverManager.getDriver().navigate().back();
-        WebDriverManager.getDriver().findElement(By.xpath("//body[1]/div[1]/section[5]/div[1]/div[2]/div[2]/div[1]/div[2]/ul[1]/li[3]/a[1]/span[1]")).click();
-        System.out.println(WebDriverManager.getDriver().getTitle());
-        WebDriverManager.getDriver().navigate().back();
-        WebDriverManager.getDriver().findElement(By.xpath("//body[1]/div[1]/section[5]/div[1]/div[2]/div[3]/div[1]/div[2]/ul[1]/li[3]/a[1]/span[1]")).click();
-        System.out.println(WebDriverManager.getDriver().getTitle());
-        WebDriverManager.getDriver().navigate().back();
-        WebDriverManager.getDriver().findElement(By.xpath("//body[1]/div[1]/section[5]/div[1]/div[2]/div[4]/div[1]/div[2]/ul[1]/li[3]/a[1]/span[1]")).click();
-        System.out.println(WebDriverManager.getDriver().getTitle());
-        WebDriverManager.getDriver().navigate().back();
-    }
-
-    @Then("All LinkedIn buttons should take to LinkedIn pages")
-    public void all_linked_in_buttons_should_take_to_linked_in_pages() {
-        WebDriverManager.getDriver().findElement(By.xpath("//body[1]/div[1]/section[5]/div[1]/div[2]/div[1]/div[1]/div[2]/ul[1]/li[4]/a[1]/span[1]")).click();
-        System.out.println(WebDriverManager.getDriver().getTitle());
-        WebDriverManager.getDriver().navigate().back();
-        WebDriverManager.getDriver().findElement(By.xpath("//body[1]/div[1]/section[5]/div[1]/div[2]/div[2]/div[1]/div[2]/ul[1]/li[4]/a[1]/span[1]")).click();
-        System.out.println(WebDriverManager.getDriver().getTitle());
-        WebDriverManager.getDriver().navigate().back();
-        WebDriverManager.getDriver().findElement(By.xpath("//body[1]/div[1]/section[5]/div[1]/div[2]/div[3]/div[1]/div[2]/ul[1]/li[4]/a[1]/span[1]")).click();
-        System.out.println(WebDriverManager.getDriver().getTitle());
-        WebDriverManager.getDriver().navigate().back();
-        WebDriverManager.getDriver().findElement(By.xpath("//body[1]/div[1]/section[5]/div[1]/div[2]/div[4]/div[1]/div[2]/ul[1]/li[4]/a[1]/span[1]")).click();
-        System.out.println(WebDriverManager.getDriver().getTitle());
-        WebDriverManager.getDriver().navigate().back();
-
-
+        List<WebElement> twitter = WebDriverManager.getDriver().findElements(By.xpath("//span/ancestor::section//a[@href='https://twitter.com']"));
+        for (int i = 0; i < twitter.size(); i++) {
+            twitter.get(i).click();
+            System.out.println(WebDriverManager.getDriver().getTitle());
+            WebDriverManager.getDriver().navigate().back();
+        }
+        List<WebElement> skype = WebDriverManager.getDriver().findElements(By.xpath("//span/ancestor::section//a[@href='https://skype.com']"));
+        for (int i = 0; i < skype.size(); i++) {
+            skype.get(i).click();
+            System.out.println(WebDriverManager.getDriver().getTitle());
+            WebDriverManager.getDriver().navigate().back();
+        }
+        List<WebElement> linkedin = WebDriverManager.getDriver().findElements(By.xpath("//span/ancestor::section//a[@href='https://linkedin.com']"));
+        for (int i = 0; i < linkedin.size(); i++) {
+            linkedin.get(i).click();
+            System.out.println(WebDriverManager.getDriver().getTitle());
+            WebDriverManager.getDriver().navigate().back();
+        }
     }
 
 }
